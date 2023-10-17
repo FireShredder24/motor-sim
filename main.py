3c12c65a280a86e841d8017b80261cf7b43de369
John Nguyen <knightofthealtar64@gmail.com>
2023-10-17 13:39:10 -0700

    Refactor to radius throughout
    Insulation erosion works as intended

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ class PressureCase:
     CASE_PRESSURE_SAFE_FACTOR = 3
     PIN_PRESSURE_SAFE_FACTOR = 3
 
-    def __init__(self, sy: float, s: float, od: float, lg: float, ta: float, rho: float, khr: float, cp: float):
+    def __init__(self, sy: float, s: float, rm: float, lg: float, ta: float, rho: float, khr: float, cp: float):
          self.ta = ta # deg F, case allowable temperature
          self.sy = sy # ksi, yield strength
          self.s = s # in, case thickness
-         self.od = od # in, case OD
-         self.id = od - 2 * s # in, case ID
+         self.rm = rm # in, case outer radius
+         self.ri = rm - s # in, case inner radius
          self.lg = lg # pressure case length
          self.rho = rho # lb / in^3, case density
-         self.mass = rho * pi * ((self.od/2)**2 - (self.id/2)**2) * self.lg # lb, case mass
+         self.mass = rho * pi * (self.rm**2 - self.ri**2) * self.lg # lb, case mass
          self.khr = khr # BTU / (hr * deg F * ft) thermal conductivity
          self.k = khr * 1 / 3600 * 1 / 12 # BTU / (s * deg F * in) thermal conductivity
          self.cp = cp # BTU / (lb * deg F) specific heat capacity
-         self.ai = 2 * pi * (self.id/2) * self.lg # in^2, internal area
-         self.Py = 2 * sy * s / od # ksi, yield pressure
+         self.ai = 2 * pi * self.ri * self.lg # in^2, internal area
+         self.Py = 2 * sy * s / (rm*2) # ksi, yield pressure
          self.Pa = self.Py / PressureCase.CASE_PRESSURE_SAFE_FACTOR # ksi, case allowable pressure
 
     def insulate(self, in_s: float, in_td: float, in_dr: float, in_rho: float, in_k: float, in_cp: float):
@@ -40,35 +40,36 @@ class PressureCase:
          self.in_cp = in_cp # BTU / (lb * deg F) insulation specific heat capacity
 
 class TempMatrixElement:
-    def __init__(self, di, od, lg, rho, Te, s):
-        self.mass = pi * ((od/2) ** 2 - (di / 2) ** 2) * rho
+    def __init__(self, ri, rm, lg, rho, Te, s):
+        self.mass = pi * (rm**2 - ri**2) * lg * rho
         self.Te = float(Te)
         self.rho = rho
-        self.di = di
-        self.od = od
+        self.ri = ri
+        self.rm = rm
         self.lg = lg
-        self.ao = 2 * pi * od / 2 * lg
-        self.ai = 2 * pi * di / 2 * lg
+        self.ao = 2 * pi * rm * lg
+        self.ai = 2 * pi * ri * lg
         self.s = s
 
     def erode(self, Td, dr, dt):
         if self.Te - Td > 0 and self.s > 0:
-            self.s -= (self.Te - Td) * dr * dt if self.s-(self.Te-Td)*dr > 0 else self.s
-            self.mass = pi * ((self.od / 2) ** 2 - (self.od - self.s)**2) * self.lg * self.rho
-            self.ai = 2 * pi * (self.od - self.s*2) / 2 * self.lg
-
+            self.s -= (self.Te - Td) * dr * dt 
+            if self.s < 0:
+                self.s = 0
+            self.mass = pi * (self.rm**2 - (self.rm - self.s)**2) * self.lg * self.rho
+            self.ai = 2 * pi * (self.rm - self.s) / 2 * self.lg
 
 def tempFlow(ki, s, A, dTi, cp, mass):
     return longdouble(ki/s*A*dTi/(cp*mass))
 
 case = PressureCase(36, 0.049, 1.5, 7.3, 900, 2.83e-1, 31.2, 1.17e-1)
-case.insulate(0.010, 300, 1e-15, 0.0361, 0.5779e-8, 2.38e-4)
+case.insulate(0.010, 300, 7e-6, 0.0361, 0.5779e-7, 2.38e-4)
 
 def heatFlowSim():
     t = 0
     dt = longdouble(1e-5)
     T = 90 # deg F, ambient temperature
-    ds = longdouble(2e-3) # diameter step
+    ds = longdouble(1e-3) # diameter step
     Tc = 2900 # deg F, chamber temperature
 
     Tmax = T # maximum temperature within the array
@@ -77,10 +78,10 @@ def heatFlowSim():
 
     elements = []
     insulation = None
-    for idx, i in enumerate(arange(case.id, case.od, ds)):
+    for idx, i in enumerate(arange(case.ri, case.rm, ds)):
         elements.append(TempMatrixElement(i, i+ds, case.lg, case.rho, T, idx*ds))
     if case.in_s:
-        insulation = TempMatrixElement(case.id - case.in_s * 2, case.id, case.lg, case.in_rho, T, case.in_s)
+        insulation = TempMatrixElement(case.ri - case.in_s * 2, case.ri, case.lg, case.in_rho, T, case.in_s)
 
     print(f"# of elements: {len(elements)}")
 
@@ -102,7 +103,7 @@ def heatFlowSim():
         max_idx = idx
 
     rt_0 = time.perf_counter()
-    while elements[0].Te <= case.ta and t <= 1:
+    while elements[0].Te <= case.ta and t <= 2:
         disp = False
         if fmod(t,0.01) < 2*dt:
             disp = True
@@ -117,7 +118,7 @@ def heatFlowSim():
                 if disp:
                     ins_s_curve.plot(t, insulation.s)
                 if insulation.s > 0:
-                    insulation.Te += tempFlow(k_flow + case.in_k / 2, insulation.s, insulation.ai, Tc - insulation.Te, case.in_cp, insulation.mass)
+                    insulation.Te += tempFlow(case.in_k, insulation.s, insulation.ai, Tc - insulation.Te, case.in_cp, insulation.mass)
                     if disp:
                         ins_T_curve.plot(t, insulation.Te)
                     k = longdouble(case.in_k)
